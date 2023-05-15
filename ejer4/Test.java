95175811b957955f1c78d920df9ea0bac426016b
Percy Carlos Diaz Tupayachi <percy.diaz@ucsm.edu.pe>
2023-05-14 20:20:05 -0500

    Pilas y Colas

---
+++ ejer4/Test.java
@@ -0,0 +1,11 @@
+package ejer4;
+
+public class Test {
+
+	public static void main(String[] args) throws ExceptionIsEmpty, ExceptionIsFull {
+		Polaca.readFile("C:\\Users\\diazt\\eclipse-workspace\\PilaCola\\src\\ejer4\\DATOSIN.txt");
+		
+
+	}
+
+}
